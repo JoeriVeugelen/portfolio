ff43b815f3662e28bdc137af6cb7339fc675233a
JoeriVeugelenPXL <joeri.veugelen@icloud.com>
2024-06-22 15:59:05 +0200

    changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ import { ThemeProvider } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Adrian's Portfolio",
-  description: "Modern & Minimal JS Mastery Portfolio",
+  title: "Joeri's Portfolio",
+  description: "My development portfolio",
 };
 
 export default function RootLayout({
